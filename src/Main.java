655a035394666f0c491ea395eec9dbde7a182117
Violetta <violet.saharova@gmail.com>
2019-12-10 20:20:52 +0300

    первый коммит

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String hello = "Hello";
+        String world = "world";
+
+        System.out.println(hello + " " + world + "!");
+    }
+}
